f584fc055a531a7904e7c7b2f211afcc2ddd62aa
Jihoon Song <spec0003@naver.com>
2022-02-09 15:27:57 +0900

    Code commit "L2-더 맵게"

---
+++ PS/prog_더 맵게.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<ll, vector<ll>, greater<ll>> pq_sco;
+
+    // pq_sco에 스코빌 지수 push
+    for (auto e : scoville)
+    {
+        pq_sco.push((ll)e);
+    }
+
+    while (1)
+    {
+        // upper k check
+        if (pq_sco.top() >= K)
+            break;
+        // fail check
+        if (pq_sco.size() < 2)
+            return -1;
+
+
+        ll small = pq_sco.top();
+        pq_sco.pop();
+        ll big = pq_sco.top();
+        pq_sco.pop();
+        pq_sco.push(small + big * 2);
+
+        answer++;
+    }
+
+    return answer;
+}
