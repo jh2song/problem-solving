e521afcf56825d5e88362813eb0600b6ca9cf553
Jihoon Song <spec0003@naver.com>
2022-02-09 15:45:02 +0900

    "L2-더 맵게" encoding issue test #3

--- PS/prog_더 맵게.cpp
+++ PS/prog_더 맵게.cpp
@@ -7,7 +7,7 @@ int solution(vector<int> scoville, int K) {
     int answer = 0;
     priority_queue<ll, vector<ll>, greater<ll>> pq_sco;
 
-    // pq_scoì— ìŠ¤ì½”ë¹Œ ì§€ìˆ˜ push
+    // pq_sco¿¡ ½ºÄÚºô Áö¼ö push
     for (auto e : scoville)
     {
         pq_sco.push((ll)e);
