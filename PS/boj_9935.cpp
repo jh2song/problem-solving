56ebe4612bffc5754da69a0647a5f412a5b51a2c
Jihoon Song <spec0003@naver.com>
2022-03-14 11:15:03 +0900

    Code commit "골4-문자열 폭발"

---
+++ PS/boj_9935.cpp
@@ -0,0 +1,60 @@
+/* 
+Âü°í
+https://cocoon1787.tistory.com/720
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+string a;
+string b;
+int a_len;
+int b_len;
+vector<char> v;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> a;
+	cin >> b;
+	a_len = a.length();
+	b_len = b.length();
+
+	for (int i = 0; i < a_len; i++)
+	{
+		v.push_back(a[i]);
+
+		if (v.size() >= b_len)
+		{
+			int v_len = v.size();
+
+			bool flag = true;
+			for (int j = 0; j < b_len; j++)
+			{
+				if (v[v_len - 1 - j] != b[b_len - 1 - j])
+				{
+					flag = false;
+					break;
+				}
+			}
+
+			if (flag)
+			{
+				for (int j = 0; j < b_len; j++)
+					v.pop_back();
+			}
+		}
+	}
+
+	if (v.empty())
+		cout << "FRULA";
+	else
+	{
+		for (auto& e : v)
+		{
+			cout << e;
+		}
+	}
+	return 0;
+}
