8b3e239e321e4b9bff57afa8cbaa194f0ca9c0a7
Jihoon Song <spec0003@naver.com>
2022-03-18 10:07:08 +0900

    Code commit "골2-std::정렬부터 시작하는 디버깅 생활"

---
+++ PS/boj_13316.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int main()
+{
+	srand(time(NULL));
+
+	vector<pii> G;
+	while (true)
+	{
+		for (int n = 2; n <= 10; n++)
+		{
+			for (int k = 0; k < 1000; k++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					G.push_back({ rand() % 1001, rand() % 1001 });
+				}
+				sort(G.begin(), G.end(), [](const pii& l, const pii& r) { return l.first * r.second < l.second* r.first; });
+
+				bool ch = false;
+				for (int i = 0; i < n; i++) {
+					for (int j = i + 1; j < n; j++) {
+						ch |= G[i].first * G[j].second > G[i].second * G[j].first;
+					}
+				}
+
+				if (ch)
+				{
+					cout << n << "\n";
+					for (auto& e : G)
+						cout << e.first << " " << e.second << "\n";
+
+					return 0;
+				}
+
+				G.clear();
+			}
+		}
+	}
+	cout << "종료되었습니다!";
+	return 0;
+}
