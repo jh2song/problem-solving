8c304384246d85fc8a7c0eaae7d59836b928d671
Jihoon Song <spec0003@naver.com>
2022-03-29 09:39:10 +0900

    encoding test

---
+++ PS/boj_2166.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<double, double> pdd;
+
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	vector<pdd> v(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i].first >> v[i].second;
+	}
+
+	// ½Å¹ß²ö °ø½Ä
+
+}
