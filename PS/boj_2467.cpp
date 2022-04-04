497bf61e5fbe824d14df3b61516cab44b7b6a56e
Jihoon Song <spec0003@naver.com>
2022-04-04 11:42:38 +0900

    Code commit "골5-용액"

---
+++ PS/boj_2467.cpp
@@ -0,0 +1,52 @@
+/*
+Âü°í
+https://m.blog.naver.com/iambigman97/221860065921
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	vector<ll> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	// first element positive
+	if (v[0] > 0)
+	{
+		cout << v[0] << " " << v[1];
+		return 0;
+	}
+	// last element negative
+	if (v[n - 1] < 0)
+	{
+		cout << v[n - 2] << " " << v[n - 1];
+		return 0;
+	}
+
+	// now this sequence has neg/pos alternative
+	int li = 0;
+	int ri = n - 1;
+	vector<pair<ll, pair<int, int>>> answer;
+	while (li < ri)
+	{
+		answer.push_back({ abs(v[li] + v[ri]), {li, ri} });
+		if (v[li] + v[ri] < 0)
+			li++;
+		else
+			ri--;
+	}
+
+	sort(answer.begin(), answer.end());
+	cout << v[answer[0].second.first] << " " << v[answer[0].second.second];
+	return 0;
+}
